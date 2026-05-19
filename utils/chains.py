0bc398510a9c8990a6e692d4c9a8887b2cab2a17
Muhammad Ali Waris Khan <MAWKHAN5031@GMAIL.COM>
2026-05-19 17:41:01 +0500

    Update chains.py

--- utils/chains.py
+++ utils/chains.py
@@ -9,20 +9,9 @@ from utils.prompts import (
 def get_gemini_response(api_key, prompt):
 
     client = genai.Client(api_key=api_key)
-    response_bool=False
-
-    try:
-
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt
     )
-    response_bool=True
-    return response.text,response_bool
-
-    except Exception as e:
-    
-        error_message = str(e)
-    
-    return error_message,response_bool
+    return response.text
 
