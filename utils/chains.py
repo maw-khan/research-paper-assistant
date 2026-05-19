8f0265af7e0cce77c0e9c9bdddbdd59199b5ae8a
Muhammad Ali Waris Khan <MAWKHAN5031@GMAIL.COM>
2026-05-19 17:07:52 +0500

    Update chains.py

--- utils/chains.py
+++ utils/chains.py
@@ -11,7 +11,7 @@ def get_gemini_response(api_key, prompt):
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-2.5-flash",
         contents=prompt
     )
 
